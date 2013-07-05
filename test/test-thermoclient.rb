7a34b3f1031eef3f0c71254644739a48c794024c
Steve Midgley <science@misuse.org>
2013-07-05 15:35:06 -0400

    Testing coverage extended, object orientation expanded in thermoclient.rb

--- test/test-thermoclient.rb
+++ test/test-thermoclient.rb
@@ -3,32 +3,71 @@ gem "minitest"
 require 'minitest/autorun'
 require '../thermoclient.rb'
 require 'fileutils'
+require 'chronic'
 
-VALID_BOOT_JSON = 'valid-thermo-boot.json.orig'
+VALID_BOOT_JSON_ORIG = 'valid-thermo-boot.json.orig'
+VALID_CONFIG_JSON_ORIG = 'backbedroom.json.orig'
+# this file must be made available to a webserver at the path specified in VALID_BOOT_JSON_ORIG for config_url and config_url_watch
+VALID_CONFIG_JSON = 'backbedroom.json'
 
-class TestMeme < Minitest::Test
+class TestThermoClient < Minitest::Test
   def setup
-    throw :TestFileNotFound unless File.exist?(VALID_BOOT_JSON)
-    FileUtils.cp(VALID_BOOT_JSON, Thermo::BOOT_FILE_NAME)
-    throw :TestFileNotFound unless File.exist?(Thermo::BOOT_FILE_NAME)
-    @config = Thermo::Configuration.new
+    FileUtils.cp(VALID_BOOT_JSON_ORIG, Thermo::BOOT_FILE_NAME)
+    FileUtils.cp(VALID_CONFIG_JSON_ORIG, VALID_CONFIG_JSON)
   end
   
   def teardown
     FileUtils.safe_unlink(Thermo::BOOT_FILE_NAME)
-    throw :TestFileNotDeleted unless !File.exist?(Thermo::BOOT_FILE_NAME)
+    FileUtils.safe_unlink(VALID_CONFIG_JSON)
+    raise Thermo::ConfigFileNotFound if File.exist?(Thermo::BOOT_FILE_NAME)
+    raise Thermo::ConfigFileNotFound if File.exist?(VALID_CONFIG_JSON)
   end
 
-  def test_boot_config_loader
-    assert_equal JSON.parse(IO.read(VALID_BOOT_JSON)), @config.boot
+  def test_configuration_loader
+    config = Thermo::Configuration.new
+    assert_equal JSON.parse(IO.read(VALID_BOOT_JSON_ORIG)), config.boot
+    assert_equal JSON.parse(IO.read(VALID_CONFIG_JSON_ORIG)), config.config
   end
 
-  def test_that_it_will_not_blend
-    skip "test"
-    #refute_match /^no/i, @meme.will_it_blend?
-  end
+  def test_thermostat
+    thermostat = Thermo::Thermostat.new
+    # Testing scenario 1:
+    #   Time: 12/1/13 5:14am
+    #   Room temp: 68
+    #   Goal temp: 62 (12:00am-6:30am)
+    #   Outcome: Heater should be off 
+puts "Scenario 1"
+    cur_time = Chronic.parse("12/1/13 5:14am")
+    cur_temp = 68
+    set_and_test_time_temp(cur_time, cur_temp, thermostat)
+    assert_heater_state_time(false, nil, thermostat)
+    thermostat.process_schedule
+    assert_heater_state_time(false, nil, thermostat)
 
-  def test_that_will_be_skipped
-    skip "test this later"
+puts "Scenario 2"
+    # Testing scenario 2:
+    #   Time: 12/1/13 5:19am
+    #   Room temp: 60
+    #   Goal temp: 62 (12:00am-6:30am)
+    #   Outcome: Heater should be on
+    cur_time = Chronic.parse("12/1/13 5:19am")
+    cur_temp = 68
+    set_and_test_time_temp(cur_time, cur_temp, thermostat)
+    assert_heater_state_time(false, nil, thermostat)
+    thermostat.process_schedule
+    assert_heater_state_time(true, cur_time, thermostat)
   end
+  
+  def assert_heater_state_time(state, last_on_time, thermostat)
+    assert_equal state, thermostat.heater_on?
+    assert_equal last_on_time, thermostat.heater_last_on_time
+  end
+  
+  def set_and_test_time_temp(cur_time, cur_temp, thermostat)
+    thermostat.override_current_time = cur_time
+    assert_equal cur_time, thermostat.current_time
+    thermostat.override_current_temp_f = cur_temp
+    assert_equal cur_temp, thermostat.current_temp_f
+  end
+  
 end
--- thermoclient.rb
+++ thermoclient.rb
@@ -1,27 +1,204 @@
 require 'json'
 require 'net/http'
+require 'chronic'
+
+# Design notes
+# the code / system that runs Thermo::Thermostat
+# must always turn the heater off when Thermostat exits
+# This is to protect against unexpected exceptions
+# that might leave the heater turned on
+
 
 module Thermo
-
-  class ConfigFileNotFound < RuntimeError; end
-
+  class ThermoError < RuntimeError; end
+  class ConfigFileNotFound < ThermoError; end
+  class UnknownSchedule < ThermoError; end
   BOOT_FILE_NAME = "boot.json"
+  HYSTERESIS_DURATION_DEFAULT = "5 minutes"
+  MAX_OPERATING_TIME_DEFAULT = "60 minutes"
+  MAX_TEMP_F_DEFAULT = 80
 
+  # Configuration class is responsible for loading and re-loading configurations
+  # It starts by loading the boot configuration from disk
+  # It then uses that booter to load a more detailed configuration file via URL
   class Configuration
     attr_accessor :boot, :config
     
     # load our default settings from the boot.json file
     def initialize(boot_file = BOOT_FILE_NAME)
+      load_boot_config_from_file(boot_file)
+      load_config_from_url
+    end
+
+    def load_boot_config_from_file(boot_file = BOOT_FILE_NAME)
       @boot = JSON.parse(IO.read(boot_file))
     end
 
-    def load_config_from_url(url = @boot.config_source.config_url)
+    def load_config_from_url(url = @boot["config_source"]["config_url"])
       @config = JSON.parse(Net::HTTP.get(URI(url)))
     end
-
   end # Configuration
 
+  # Thermostat class is responsible for getting a configuration file from 
+  # Configuration class and interpreting it to control the RPi hardware
+  # It must correctly read temperature data from RPi, understand scheduling
+  # and write relay actions to RPi based on schedule and temperature goal and 
+  # actual data
+  class Thermostat
+    attr_accessor :configuration
+
+    # safety features
+    # holds a time which indicates when the heater is allowed to turn back on
+    # this prevents hystersis, where the heater goes on/off rapidly based on
+    # small temperature fluctuations
+    attr_accessor :max_heater_on_time
+    attr_accessor :max_temp_f
+    attr_accessor :hysteresis_duration
+    
+    # used for testing - if set, we use this value instead of system values
+    attr_accessor :override_current_time
+    attr_accessor :override_current_temp_f
+    attr_accessor :override_relay_state
+    attr_reader :heater_last_on_time
+    attr_reader :goal_temp
+    
+    # start up by loading our configuration
+    def initialize
+      # turn heater off as first initializing step
+      # this is to protect against a recurring crash
+      # leaving the heater in the permenantly on condition
+      set_heater_state(false)
+      @configuration = Configuration.new
+      setup_safety_config
+    end
+    
+    def setup_safety_config
+      @max_heater_on_time = self.configuration.boot["operating_parameters"]["max_operating_time"]
+      if !self.parse_time(@max_heater_on_time+ " from now").kind_of?(Time)
+        @max_heater_on_time = MAX_OPERATING_TIME_DEFAULT
+      end
+      @hysteresis_duration = self.configuration.boot["operating_parameters"]["hysteresis_duration"]
+      if !self.parse_time(@hysteresis_duration+ " from now")
+        @hysteresis_duration = HYSTERESIS_DURATION_DEFAULT 
+      end
+      @max_temp_f = self.configuration.boot["operating_parameters"]["max_temp_f"]
+      if !@max_temp_f.is_a? Integer
+        @max_temp_f = MAX_TEMP_F_DEFAULT
+      end
+    end
+
+    def current_time
+      @override_current_time || Time::now
+    end
+
+    def current_hour_min_sec_str
+      cur_time = self.current_time
+      hour = cur_time.hour
+      min = cur_time.min
+      sec = cur_time.sec
+      "#{hour}:#{min}:#{sec}"
+    end
+
+    def current_year_month_day_str
+      cur_time = self.current_time
+      year = cur_time.year
+      month = cur_time.month
+      day = cur_time.day
+      "#{year}-#{month}-#{day}"
+    end
+    
+    # Returns Chronic-parsed Time object
+    # we set the current time to base the parsing on 
+    # using our internal time, which permits testing
+    # using alternate times to the current system clock
+    def parse_time(time_str)
+      Chronic.parse(time_str, :now => Chronic.parse(self.current_year_month_day_str))
+    end
+    
+    # assigns hardware-obtained temperature in farenheit to internal state variable
+    def current_temp_f
+      #TODO hardware call to obtain temperature reading
+      @override_current_temp_f # || get_hw_temp_f
+    end
+    
+
+    # we process the schedule periodically - this function uses current
+    # time & temp, and compares to scheduled time and temp to determine
+    # the action it should take
+    def process_schedule
+      # error out if we don't know how to handle the schedule file
+      raise UnknownSchedule, "Unknown schedule found in configuration file. Currently we only handle 'daily_schedule' type" unless self.configuration.config["daily_schedule"]
+
+      # safety checks
+      # heater should not run more than max heater on time
+      # heater should not run hotter than max temp
+      
+      # compare current time/temp with config specified in file
+      self.configuration.config["daily_schedule"]["daily"]["times_of_operation"].each do |time_window|
+        start_time = self.parse_time(time_window["start"])
+        end_time = self.parse_time(time_window["stop"])
+        goal_temp = time_window["temp_f"]
+
+        if (start_time < current_time) && (end_time > current_time)
+          if self.current_temp_f < goal_temp
+            set_heater_state(true)
+          else
+            set_heater_state(false)
+          end
+        end
+      end
+    end
+    
+    # this records the time at which the heater was last turned on
+    # used when heater is turned on
+    def set_heater_last_on_time
+      @heater_last_on_time = self.current_time
+    end
+
+    # this clears the time at which the heater was last turned on
+    # used when heater is turned off
+    def reset_heater_last_on_time
+      @heater_on_time = nil
+    end
+
+    def heater_on?
+      # TODO add hardware call to determine relay state
+      # get_hardware_heater_activity_state || _
+      @override_heater_activity_state  || @heater_on
+    end
+
+    def heater_on=(state)
+      @heater_on = state
+      # TODO add hardware call to set heater relay to on or off
+    end
+
+    # Returns true if the heater should remain off due to hysteresis control
+    # This prevents the heater from going on and off too rapidly
+    def in_hysteresis?
+      # we don't use "Time::now" here b/c current_time may be overridden in tests
+      time = self.current_hour_min_sec_str
+      # this calculate the current time plus the hysteresis duration
+      hys_window = self.parse_time(@hysteresis_duration+ " from "+time) 
+      current_time >= hys_window
+    end
+
+    # send true to turn heater on, false to turn heater off
+    def set_heater_state(setting = true)
+      # Only change the hardware state to on if hysteresis_window is satisfied
+      if setting && !in_hysteresis?
+        heater_on = true
+        set_heater_last_on_time
+      elsif !setting 
+        heater_on = false
+        reset_heater_last_on_time
+      end
+    end
+
+  end # Thermostat
+
 end # Thermo
+
+
 
 
 =begin
@@ -105,8 +282,11 @@ url and then hang out on config-url-watch will update immediately upon remote fi
 
 schedule notes:
   If there is a gap between one end time and the following start time, heater will be OFF during that period
-  If a scheduled time extends to the following day (or over a following time period of the same day),
-    it overrides any settings for the following day/time period until it stops/expires
+  If a scheduled time extends to the following day (or over a following time period of the same day), it will be overridden by any settings for the following day/time period that overlap. It will function if the earliest time the following day is not overlapping.
+    In short, the first matching time to the current time will be processed
+    So: 
+    6:30am-9:00pm followed by 9:00pm-7:30am is the same as
+    6:30am-9:00pm followed by 9:00pm-6:30am
 */
 
 
